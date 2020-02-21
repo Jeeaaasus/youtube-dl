fed32abf3aa71cffe343230dd9ef0c92a1b623a1
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-02-21 15:34:26 +0100

    Update youtube-dl.sh

--- root/app/youtube-dl/youtube-dl.sh
+++ root/app/youtube-dl/youtube-dl.sh
@@ -2,7 +2,7 @@
 
 echo "youtube-dl version: $(youtube-dl --version)"
 echo "checking for updates..."
-python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade youtube_dl > /dev/null
+python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade youtube_dl --user > /dev/null
 
 if [ -f "/config/archive.txt" ]
 then
