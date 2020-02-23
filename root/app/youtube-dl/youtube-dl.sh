351111c2cded6ef5e341b05941f87f4bc612fcb1
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-02-23 19:20:30 +0100

    Update youtube-dl.sh

--- root/app/youtube-dl/youtube-dl.sh
+++ root/app/youtube-dl/youtube-dl.sh
@@ -5,6 +5,7 @@ echo "youtube-dl version: $(youtube-dl --version)"
 
 if grep -qe '--format ' "/config/args.conf"; then youtubedl_args_format=true; else youtubedl_args_format=false; fi
 if grep -qe '--dateafter ' "/config/args.conf"; then youtubedl_args_dateafter=true; else youtubedl_args_dateafter=false; fi
+if grep -qe '--download-archive ' "/config/args.conf"; then youtubedl_args_downloadarchive=true; else youtubedl_args_downloadarchive=false; fi
 
 if [ -f "/config/archive.txt" ]
 then
@@ -18,25 +19,34 @@ then
     fi
   fi
 else
-  echo "'archive.txt' not found."
-  echo "creating 'archive.txt'."
-  touch "/config/archive.txt"
-  if ! $youtubedl_args_dateafter
+  if ! $youtubedl_args_downloadarchive
   then
-    echo "'--dateafter' not found in args.conf"
-    echo "creating temporary 'dateafter.txt'."
-    date --date "-1 days" "+%Y%m%d" > "/config/dateafter.txt"
+    echo "'archive.txt' not defined in args."
+    echo "creating 'archive.txt'."
+    touch "/config/archive.txt"
+    if ! $youtubedl_args_dateafter
+    then
+      echo "'--dateafter' not found in args.conf"
+      echo "creating temporary 'dateafter.txt'."
+      date --date "-1 days" "+%Y%m%d" > "/config/dateafter.txt"
+    fi
   fi
 fi
 
+youtubedl_exec="youtube-dl"
+if ! $youtubedl_args_downloadarchive; then youtubedl_exec+=" --download-archive "/config/archive.txt""; fi
+if ! $youtubedl_args_dateafter; then youtubedl_exec+=" --dateafter $(cat /config/dateafter.txt)"; fi
+youtubedl_exec+=" --config-location "/config/args.conf""
+youtubedl_exec+=" --batch-file "/config/channels.txt""
+
 youtubedl_last_run_date=$(date "+%s")
 
-youtube-dl \
-  $(if ! $youtubedl_args_format; then echo "--format "bestvideo[height<=$youtubedl_quality][vcodec=vp9][fps>30]+bestaudio[acodec!=opus] / bestvideo[height<=$youtubedl_quality][vcodec=vp9]+bestaudio[acodec!=opus] / bestvideo[height<=$youtubedl_quality]+bestaudio[acodec!=opus]" \\"; fi)
-  $(if ! $youtubedl_args_dateafter; then echo "--dateafter "$(cat "/config/dateafter.txt")" \\"; fi)
-  --config-location "/config/args.conf" \
-  --download-archive "/config/archive.txt" \
-  --batch-file "/config/channels.txt"
+if ! $youtubedl_args_format
+then
+  $youtubedl_exec --format 'bestvideo[height<=$youtubedl_quality][vcodec=vp9][fps>30]+bestaudio[acodec!=opus] / bestvideo[height<=$youtubedl_quality][vcodec=vp9]+bestaudio[acodec!=opus] / bestvideo[height<=$youtubedl_quality]+bestaudio[acodec!=opus]'
+else
+  $youtubedl_exec
+fi
 
 if [ $(( ($(date "+%s") - $youtubedl_last_run_date) / 60 )) -ge 2 ]
 then
@@ -44,6 +54,7 @@ then
 else
   echo "$(date "+%Y-%m-%d %H:%M:%S") - execution took $(( ($(date "+%s") - $youtubedl_last_run_date) )) seconds"
 fi
+
 echo "waiting $youtubedl_interval.."
 sleep $youtubedl_interval
 date "+%Y-%m-%d %H:%M:%S"
