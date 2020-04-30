2ad5cd524b2a826ed584e38f3451fd9c225280be
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-04-30 21:07:26 +0200

    Update updater.sh

--- root/app/youtube-dl/updater.sh
+++ root/app/youtube-dl/updater.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv bash
 
-apk update > /dev/null && apk upgrade > /dev/null
-python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade youtube_dl > /dev/null
+#apk update > /dev/null && apk upgrade > /dev/null
+#python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade youtube_dl > /dev/null
+apk update > /dev/null && apk upgrade
+python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade youtube_dl
 
 sleep 60m
