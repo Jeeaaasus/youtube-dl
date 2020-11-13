429bf266651e994683f95a150b9de2ad24f30ee4
Jeeaaasus <15081909+Jeeaaasus@users.noreply.github.com>
2020-11-13 21:18:18 +0100

    Update updater.sh

--- root/app/youtube-dl/updater.sh
+++ root/app/youtube-dl/updater.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bash
 
-sleep 3h
 apk update > /dev/null && apk upgrade > /dev/null
 python3 -m pip --no-cache-dir --disable-pip-version-check install --upgrade git+https://github.com/blackjack4494/yt-dlc > /dev/null
+sleep 3h
